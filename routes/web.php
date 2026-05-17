c9946f8f0f1ca5da8e2acf19865ce53cb2db4202
L-U-P-I-N <skjccm@gmail.com>
2026-05-17 05:10:13 +0300

    Remove temporary migration route

--- routes/web.php
+++ routes/web.php
@@ -5,12 +5,3 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-
-Route::get('/run-migrations-123', function () {
-    try {
-        \Illuminate\Support\Facades\Artisan::call('migrate', ['--force' => true]);
-        return 'Migrations executed successfully! <br><pre>' . \Illuminate\Support\Facades\Artisan::output() . '</pre>';
-    } catch (\Exception $e) {
-        return 'Error: ' . $e->getMessage();
-    }
-});
